dec156caabfd166c4488557ce5e9fa14ad47d4bd
yunduo <2992588618@qq.com>
2022-09-15 20:28:36 +0800

    初始页面布局

---
+++ SigninClass/app/src/main/java/com/example/signinclass/Constants.java
@@ -0,0 +1,7 @@
+package com.example.signinclass;
+
+public final class Constants {
+    public Constants() {}
+
+    public static String Server_URL="http://47.107.52.7:88/member/";
+}
---
+++ SigninClass/app/src/main/java/com/example/signinclass/ui/course/CourseViewModel.java
@@ -0,0 +1,18 @@
+package com.example.signinclass.ui.course;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class CourseViewModel extends ViewModel {
+    private final MutableLiveData<String> mText;
+
+    public CourseViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is course fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
