2efdde565b405c04939b7e13eb783a6778d53a6c
kwjsoiq <2927951393@qq.com>
2022-09-25 23:31:46 +0800

    修改个人主页布局

---
+++ SigninClass/app/src/main/java/com/example/signinclass/ui/home/HomeEdit.java
@@ -0,0 +1,16 @@
+package com.example.signinclass.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.signinclass.R;
+
+public class HomeEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_edit);
+    }
+}
