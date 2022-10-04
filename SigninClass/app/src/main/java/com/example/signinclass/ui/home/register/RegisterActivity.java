eafb7b96eff949fa3907dd8349e0da8aa48a87d9
kwjsoiq <2927951393@qq.com>
2022-10-04 15:25:24 +0800

    登录和注册界面布局

---
+++ SigninClass/app/src/main/java/com/example/signinclass/ui/home/register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.signinclass.ui.home.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.signinclass.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
