636567ff807a7d12d5316295e20d93bb23921332
kwjsoiq <2927951393@qq.com>
2022-10-07 20:57:43 +0800

    注册

--- SigninClass/app/src/main/java/com/example/signinclass/ui/home/register/RegisterActivity.java
+++ SigninClass/app/src/main/java/com/example/signinclass/ui/home/register/RegisterActivity.java
@@ -3,14 +3,24 @@ package com.example.signinclass.ui.home.register;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.signinclass.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText username = null;
+    EditText Pwd = null;
+    EditText RPwd = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.et_username);
+        Pwd = findViewById(R.id.et_pwd);
+        RPwd = findViewById(R.id.et_pwd_repeat);
+
     }
 }
